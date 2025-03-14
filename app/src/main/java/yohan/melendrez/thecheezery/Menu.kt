b363bc4662c3d3ca59f17a411b6f76aa3c0b2720
Gabriel <140923456+Yohan-Melendrez@users.noreply.github.com>
2025-03-13 19:41:31 -0700

    S

---
+++ app/src/main/java/yohan/melendrez/thecheezery/Menu.kt
@@ -0,0 +1,48 @@
+package yohan.melendrez.thecheezery
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Menu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu)
+        val btncold: Button = findViewById(R.id.botonCold)
+        val btnHot: Button = findViewById(R.id.botonHot)
+        val btnSweets: Button = findViewById(R.id.botonSweets)
+        val btnSalties: Button = findViewById(R.id.botonSalties)
+
+        btncold.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Antojitos")
+            startActivity(intent)
+        }
+        btnHot.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Especialidades")
+            startActivity(intent)
+        }
+        btnSweets.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Com")
+            startActivity(intent)
+        }
+        btnSalties.setOnClickListener {
+            val intent = Intent(this, ProductosActivity::class.java)
+            intent.putExtra("menuType", "Tortas")
+            startActivity(intent)
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
