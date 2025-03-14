f94544b89d85bf85fd0db64a24fb9d121f363f9a
Gabriel <140923456+Yohan-Melendrez@users.noreply.github.com>
2025-03-13 21:06:35 -0700

    .

--- app/src/main/java/yohan/melendrez/thecheezery/detalle_producto.kt
+++ app/src/main/java/yohan/melendrez/thecheezery/detalle_producto.kt
@@ -1,6 +1,8 @@
 package yohan.melendrez.thecheezery
 
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,14 @@ class detalle_producto : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_detalle_producto)
+        val imagen: ImageView = findViewById(R.id.img_Producto)
+        val precio: TextView = findViewById(R.id.costo)
+
+        val bundle = intent.extras
+        if (bundle != null) {
+            imagen.setImageResource(bundle.getInt("imagen"))
+           precio.text = bundle.getDouble("costo").toString()
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
